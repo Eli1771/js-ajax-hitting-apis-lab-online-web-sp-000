c8da2cb6f6bdf10c38da8ca429dbc9b18944c020
elijahmm17 <elijahmm17@gmail.com>
2019-10-02 18:13:06 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ function getRepositories() {
   req.addEventListener('load', displayRepositories);
   req.open('GET', 'https://api.github.com/users/' + username + '/repos');
   req.send();
-  //return false;
 }
 
 function displayRepositories() {
