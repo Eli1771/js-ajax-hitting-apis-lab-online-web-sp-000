f6ddd57cf89d0d1ee97c9765decfaee75114ecef
elijahmm17 <elijahmm17@gmail.com>
2019-10-02 17:48:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
-// your code here
+//your code here
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  const username = document.querySelector('input#username').value;
+  
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', 'https://api.github.com/users/' + username + '/repos');
+  req.send();
+  return false;
+}
+
+function displayRepositories() {
+  let repos = JSON.parse(this.responseText);
+  console.log(repos);
+  
+  let repoList = `<ul>
+    ${repos.map(repo => {
+      return `<li>
+        <h2>${repo.html_url}</h2>
+        <a href="getCommits(); return false;">Show Commits</a>
+        <a href="getBranches(); return false;">Show Branches</a>
+      </li>`;
+    }).join('')}
+  </ul>`;
+  document.querySelector('div#repositories').innerHTML = repoList;
+}
+
+function getCommits() {
+  const req = new XMLHttpRequest();
+  
+}
+
+function displayCommits() {
+  
+}
+
+function getBranches() {
+  
+}
+
+function displayBranches() {
+  
+}
