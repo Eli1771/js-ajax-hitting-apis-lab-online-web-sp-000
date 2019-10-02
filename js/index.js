ddf226e8f7db52097e22fd3900864918e07c3563
elijahmm17 <elijahmm17@gmail.com>
2019-10-02 18:12:58 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -32,6 +32,7 @@ function getCommits(el) {
   
   req.addEventListener('load', displayCommits);
   req.open('GET', `https://api.github.com/repos/${ownerName}/${repoName}/commits`);
+  req.send();
 }
 
 function displayCommits() {
